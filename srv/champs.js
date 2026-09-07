a01d916c94f3f98d17dc32a7787c2e784c9d4a59
marcin.pciak@knauf.com <marcin.pciak@knauf.com>
2026-09-07 21:44:34 +0000

    project

---
+++ srv/champs.js
@@ -0,0 +1,15 @@
+const cds = require('@sap/cds')
+const path = require('node:path');
+const fs = require('node:fs');
+module.exports = class SAPDevChamps extends cds.ApplicationService { init() {
+
+  this.on('show', function(req){
+    const filePath = path.join(__dirname, '../db/data/champions.txt');
+    const fileContent = fs.readFileSync(filePath, 'utf8');
+    const champs = fileContent.split('\n').map(line => line.trim());
+ 
+    return champs;
+  });
+
+  return super.init()
+}}
