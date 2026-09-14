cd308554b5b193ae1563dce598998ef6e6c7c814
marcin.pciak@knauf.com <marcin.pciak@knauf.com>
2026-09-14 16:29:39 +0000

    file access

--- srv/champs.js
+++ srv/champs.js
@@ -4,8 +4,8 @@ const fs = require('node:fs');
 module.exports = class SAPDevChamps extends cds.ApplicationService { init() {
 
   this.on('show', function(req){
-    // const filePath = path.join(__dirname, '../db/data/champions.txt');
-    const filePath = '../db/data/champions.txt';
+    const filePath = path.join(__dirname, './champions.txt');
+    // const filePath = '../db/data/champions.txt';
     const fileContent = fs.readFileSync(filePath, 'utf8');
     const champs = fileContent.split('\n').map(line => line.trim());
  
